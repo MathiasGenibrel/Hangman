d625726101b1b4e27a1e396200db34d2705be98a
MathiasGenibrel <mathias.geni@gmail.com>
2022-10-28 14:58:56 +0200

    ♻️ Change Class name:
    Create Human class (Human is children of Player)
    Transform Player class in Abstract class

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from models.Hangman import Hangman
-from models.Player import Player
+from models.Human import Human
 from models.AiRandom import AiRandom
 
-game_master = Player("Alfred")
-# guesser = Player("Greg")
+game_master = Human("Alfred")
+# game_master = AiRandom()
+# guesser = Human("Greg")
 guesser = AiRandom()
 
 Hangman(game_master=game_master, guesser=guesser).start_game()
